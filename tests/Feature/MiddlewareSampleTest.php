887bda9d71d17e04a778831518020856c9530a76
kingsley achumie <kingsonly13c@gmail.com>
2024-07-20 19:12:35 +0100

    wip

--- tests/Feature/MiddlewareSampleTest.php
+++ tests/Feature/MiddlewareSampleTest.php
@@ -10,18 +10,18 @@ class MiddlewareSampleTest extends TestCase
 {
     use RefreshDatabase, WithFaker;
 
-    public function test_protected_route_allows_access_with_valid_scope(): void
-    {
-        $this->actingAsAuthenticatedTestUser();
-        $response = $this->getJson('/api/protected');
-        $response->assertStatus(200);
-        $response->assertJson(['message' => 'Access granted']);
-    }
+    // public function test_protected_route_allows_access_with_valid_scope(): void
+    // {
+    //     $this->actingAsAuthenticatedTestUser();
+    //     $response = $this->getJson('/api/protected');
+    //     $response->assertStatus(200);
+    //     $response->assertJson(['message' => 'Access granted']);
+    // }
 
-    public function test_protected_route_denies_access_with_invalid_scope(): void
-    {
-        $this->actingAsUnAuthenticatedTestUser();
-        $response = $this->getJson('/api/protected');
-        $response->assertStatus(401);
-    }
+    // public function test_protected_route_denies_access_with_invalid_scope(): void
+    // {
+    //     $this->actingAsUnAuthenticatedTestUser();
+    //     $response = $this->getJson('/api/protected');
+    //     $response->assertStatus(401);
+    // }
 }
